1e669a57e0ec4148acbb3cb3a7785d825cda00cf
Julian <76625941+Epiczhul@users.noreply.github.com>
2025-10-12 19:00:10 +0200

    Update psram.h

--- main/psram.h
+++ main/psram.h
@@ -1,3 +1,6 @@
+// ============================================================================
+// psram.h - Updated for internal PSRAM
+// ============================================================================
 /*
  * Copyright (c) 2023, Jisheng Zhang <jszhang@kernel.org>. All rights reserved.
  *
@@ -7,8 +10,15 @@
 #ifndef PSRAM_H
 #define PSRAM_H
 
+#include <stdint.h>
+#include <stddef.h>
+
 int psram_init(void);
 int psram_read(uint32_t addr, void *buf, int len);
 int psram_write(uint32_t addr, void *buf, int len);
 
+void *psram_get_base(void);
+size_t psram_get_size(void);
+
 #endif /* PSRAM_H */
+
